cfc80ccefd0863b6eccd52d6f94b909fe6a69783
Pavel Poronko <me@gibzzz.tk>
2016-09-03 20:04:54 +0700

    JWT and LICENSE

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from eve import Eve
 from eve_elastic import Elastic
 from eve_swagger import swagger
+from eve_auth_jwt import JWTAuth
 import config
 
 app = Eve(settings=config.app, data=Elastic)
