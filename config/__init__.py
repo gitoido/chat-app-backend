36846c24556d8212969c48f0d46d52ec1eeab32e
Pavel Poronko <me@gibzzz.tk>
2016-09-03 19:51:28 +0700

    CORS

--- config/__init__.py
+++ config/__init__.py
@@ -3,10 +3,11 @@ import domain
 import swagger
 
 app = {
-    'DOMAIN': domain.domain,
     'SWAGGER_INFO': swagger.api_doc_info,
     'ELASTICSEARCH_URL': os.getenv('BONSAI_URL', 'http://localhost:9200/'),
     'ELASTICSEARCH_INDEX': 'chat',
     'RESOURCE_METHODS': ['GET', 'POST', 'DELETE'],
-    'ITEM_METHODS': ['GET', 'PATCH', 'PUT', 'DELETE']
+    'ITEM_METHODS': ['GET', 'PATCH', 'PUT', 'DELETE'],
+    'X_DOMAINS': '*',
+    'DOMAIN': domain.domain,
 }
