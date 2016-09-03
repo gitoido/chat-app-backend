8447275c97107636dbce3aff032f45f613822ba5
Pavel Poronko <me@gibzzz.tk>
2016-09-03 19:02:01 +0700

    Swagger and storage - missing files

---
+++ config/swagger.py
@@ -0,0 +1,14 @@
+api_doc_info = {
+    'title': 'My Supercool API',
+    'version': '1.0',
+    'description': 'an API description',
+    'termsOfService': 'my terms of service',
+    'contact': {
+        'name': 'nicola',
+        'url': 'http://nicolaiarocci.com'
+    },
+    'license': {
+        'name': 'BSD',
+        'url': 'https://github.com/nicolaiarocci/eve-swagger/blob/master/LICENSE',
+    }
+}
